e33723c3fda59cfc691588c3d8471cdf813f8abe
Stephen Webber <offgridauthor@gmail.com>
2020-12-23 17:10:10 -0800

    added instructions constructor, thumbnail functionality and prepared for postcard input

--- js/app.js
+++ js/app.js
@@ -1,60 +1,59 @@
 'use strict';
 
 // GLOBAL VARIABLES --------
-
+var pickedPlace = [];
 var maxScenes = 5;
 var actualScenes = 0;
 var userName = '';
 var allDestinations = [];
 var remainingPlaces = []; //This array is used to populate the thumnails of remaining choices
 var chosenPlaces = [];
+var allInstructions = [];
 
 // DOM IDS --------
-
 var mainContainer = document.getElementById('maincontainer'); // where the action happens
+var selectionContainer = document.getElementById('selectioncontainer'); // for 10 destination thumbnails
+var headerContainer = document.getElementById('headercontainer'); // for active image name
+var instructionsContainer = document.getElementById('instructionscontainer');
 var imageContainer = document.getElementById('imagecontainer'); // for initial image and active destination image
 var descriptionContainer = document.getElementById('description'); // for text instructions 
-var eventContainer = document.getElementById('formcontainer'); // for both forms 
-var selectionContainer = document.getElementById('selectioncontainer'); // for 10 destination thumbnails
+var eventContainer = document.getElementById('eventcontainer'); // for both forms 
 
-// CONSTRUCTOR --------
-
+// CONSTRUCTORS --------
 function Adventure(name, image, text, teaser) {
   this.name = name;
   this.image = `img/${image}`;
   this.thumbnail = `thumbs/${image}`;
   this.text = text;
- // this.blurb = blurb;
+  // this.blurb = blurb;
   this.teaser = teaser;
   allDestinations.push(this);
 }
 
-// DESTINATION INSTANCES -------
+function Instructions(name, image, text) {
+  this.name = name;
+  this.image = `img/${image}`;
+  this.text = text;
+  allInstructions.push(this);
+}
 
+// INSTANCES -------
 new Adventure('Space Needle', 'space-needle.png', 'Long Descriptive Text about it', 'Click this awesome choice!');
-new Adventure('Seattle Sunset', 'seattle-sunset.png', 'Long Descriptive Text about it', 'Click this awesome choice!');
-new Adventure('Great Wheel', 'great-wheel.png', 'Long Descriptive Text about it', 'Click this awesome choice!');
-new Adventure('Pike Place', 'pike-place.png', 'Long Descriptive Text about it', 'Click this awesome choice!');
-new Adventure('Waterfront', 'waterfront.png', 'Long Descriptive Text about it', 'Click this awesome choice!');
+new Adventure('Seattle Sunset', 'seattle-sunset.png', 'Long Descriptive Text about SEATTLE', 'Click this awesome choice!');
+new Adventure('Great Wheel', 'great-wheel.png', 'Long Descriptive Text about WHEEL', 'Click this awesome choice!');
+new Adventure('Pike Place', 'pike-place.png', 'Long Descriptive Text about PIKE', 'Click this awesome choice!');
+new Adventure('Waterfront', 'waterfront.png', 'Long Descriptive Text about WATERFRONT', 'Click this awesome choice!');
 
+// for landing page, how to play page, and pre-postcard page
+
+new Instructions('Welcome to our Adventure in Seattle Game!', 'starting-image.png', `If you choose to play, you'll be led on a virtual adventure around the city, to see whichever sights you'd like to see. You'll learn fun facts and trivia about each location along the way. At the end, you'll have a memento from your trip based on where you decided to go! Enjoy your time, hope you love Seattle!`)
+new Instructions('Your Custom Postcard Awaits. . .', 'prepostcard.png', 'Click the button to see your memento')
 
 // to neaten up this instantiation we could make the text / blurb/ teasers all variables that get filled by functions. (reference Pheasants demo for this)
 
 // GLOBAL FUNCTIONS ----------
-
-// userName stores user form entry. After game completion, userName, postcard text and 5 destination choices are stored to localstorage.
-
-// Adventure constructor gets fed 10 destinations to instantiate. each Adventure destination instance has these properties: image, text, description, mouseover teaser text, and this.push to allDestinations array.
-
-// allDestinations stores these instances
-
-// remainingPlaces array stores the images that show as available in the thumbnails
-
-// Each destination that is selected gets pushed into chosenPlaces array. These images display as grayed out from other thumbnails. Potentially these images are popped from remainingPlaces
-
 function renderElement(newElement, parentElement, obj, content, index) {
-  // this function populates DOM elements so we don't have to write all this out for each one
-  if (newElement === 'img' && content === 'thumbnail') {
+  if (newElement === 'img' && content === 'thumbnail') { // for thumbnails
     var childElement = document.createElement(newElement);
     childElement.src = obj.thumbnail;
     childElement.title = obj.teaser;
@@ -62,90 +61,87 @@ function renderElement(newElement, parentElement, obj, content, index) {
     childElement.setAttribute('id', index);
     parentElement.appendChild(childElement);
     childElement.addEventListener('click', handleClick);
-  } else if (newElement === 'img' && content === 'image') { 
+  } else if (newElement === 'img' && content === 'image') {  // for large images
+    parentElement.innerHTML = '';
     var childElement = document.createElement(newElement);
     childElement.src = obj.image;
     childElement.title = obj.teaser;
     childElement.alt = obj.name;
     parentElement.appendChild(childElement);
   } else {
-    var childElement = document.createElement(newElement);
+    parentElement.innerHTML = '';
+    var childElement = document.createElement(newElement);  // for text 
     childElement.textContent = obj[content];
     parentElement.appendChild(childElement);
   }
 }
 
-populateDestinations();
-renderElement('img', imageContainer, allDestinations[0], 'thumbnail');
-renderElement('img', imageContainer, allDestinations[0], 'image');
-renderElement('p', imageContainer, allDestinations[0], 'text');
-
-
 function renderThumbnails() {
-  //delete previously selected.
   selectionContainer.innerHTML = '';
-//iterate through array each time a new thing gets selected. 
-  // need img source
-  // use the title to find the actual object
-  // in selectionContainer
   for (var i = 0; i < remainingPlaces.length; i++) {
     renderElement('img', selectionContainer, remainingPlaces[i], 'thumbnail', i);
   }
 }
 
-renderThumbnails();
-
-function populateDestinations() {
+function populateDestinations() { // preserves original list of destinations and creates a working copy for user interaction
   for (var i = 0; i < allDestinations.length; i++) {
     remainingPlaces[i] = allDestinations[i];
   }
 }
 
-
-
-
 function nameInput() {
+  // userName stores user form entry. After game completion, userName, postcard text and 5 destination choices are stored to localstorage.
+
   // nameInput() function receives userName form submission, clear away form, and reverals thumbnails 
 
+  userName = document.getElementById("username");
+  localStorage.setItem("username", userName.value);
+  console.log(userName.value);
 }
 
 function beginAdventure() {
-
- // display thumbnails and starting image and give directions
- 
+  //  run nameInput() to get user name via form.
+  //  clear away form
+  // then run showNextScene() to play game.
+  nameInput();
+  populateDestinations();
+  // display starting image and give directions
+  renderElement('h2', headerContainer, allInstructions[0], 'name');
+  renderElement('img', imageContainer, allInstructions[0], 'image');
+  renderElement('p', descriptionContainer, allInstructions[0], 'text');
+  renderThumbnails();
 }
 
 function showNextScene() {
-  // showNextScene() function clears away previous image (if one previously there) and text and render text and image in appropriate sections in DOM. and presents the choices in thumbnails./updates them if selected.
-
-}
-
-function showAdventure() {
-  // first, run nameInput() to get user name via form.
-  // then, clear away form
-  // then run showNextScene() to play game.
-
+  renderElement('h2', headerContainer, pickedPlace[0], 'name');
+  renderElement('img', imageContainer, pickedPlace[0], 'image');
+  renderElement('p', descriptionContainer, pickedPlace[0], 'text');
 }
 
 function postcardInput() {
-  // postcardInput() reveals a text input form. Upon submission, runs revealPostcard()
-
+  eventContainer.innerHTML = 'Whew! -- you have completed your journey of Seattle. Each step of your journey has been commemorated by a photo on your custom postcard.';
+  // generate a user input form for the text they'd like to put on the postcard
+  var postcardButton = document.createElement('button');
+  eventContainer.appendChild(postcardButton);
+  postcardButton.textContent = 'Click Here for Your Custom Postcard';
+  postcardButton.addEventListener('click', revealPostcard);
 }
 
+function fillContainerWithSelectedImages() { //not sure why this doesn't grab the image thumbnails
+  for (var i = 0; i < chosenPlaces.length; i++) {
+    renderElement('img', eventContainer, chosenPlaces[i], 'thumbnail', i);
+  }
+}
 
 function revealPostcard() {
   // User is sent to results page revealing the postcard.
-  // The user is taken through a total of 5 scenes. 
-  // revealPostcard() function displays thumbnails from chosenPlaces array, username, and postcardinput form text (during the game). Then generates a link to load results.html to see all previous postcards as well, ranked in order of appearance (timestamp?).
-
-  // On results page, a DOM p element is created within the postcard with that text. (Max characters on form entry to fit on postcard.)
-
+  // revealPostcard() function displays thumbnails from chosenPlaces array, username, and postcardinput form text (during the game). Then generates a link to load postcard.html to see all previous postcards as well, ranked in order of appearance (plus a timestamp/how long it took?).
+  // On results page, a DOM p element is created within the postcard with that text. (limit Max characters on form entry to fit on postcard.)
 }
 
 // EVENT HANDLERS ----------
 
-//Event handler seems to be not connected to the section of images, does not pick up individual image click
-function handleClick(event) {
+function handleClick(event) {   // user has clicked thumbnail to choose next destination.
   actualScenes++;
   var clickedDestination = event.target.alt;
   var popIndex = null;
@@ -154,19 +150,14 @@ function handleClick(event) {
       popIndex = i;
     }
   }
-  var pickedPlace = remainingPlaces.splice(popIndex, 1);
+  pickedPlace = remainingPlaces.splice(popIndex, 1);
   chosenPlaces.push(pickedPlace);
-
-  // user has clicked thumbnail to choose next destination.
-
-  if (actualScenes === maxScenes) {
-    // selection ends; users is presented with form 
+  renderElement('img', eventContainer, pickedPlace[0], 'thumbnail');
+  if (actualScenes === maxScenes) {    // selection ends after 5 are chosen; users is presented with postcard form 
     postcardInput();
   }
-  // pop from available array and push to chosen array
-  // showNextScene(allDestinations.name); // find object by using one of its properties (object.title)
   renderThumbnails();
+  showNextScene();
 }
 
-showAdventure();
-// selectionContainer.addEventListener('click', handleClick);
+beginAdventure();
