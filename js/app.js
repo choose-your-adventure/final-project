ceab6293739c82f20eb8310152975a5ed9eec678
Stephen Webber <offgridauthor@gmail.com>
2020-12-24 00:49:06 -0800

    worked with input forms

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// GLOBAL VARIABLES --------
 var pickedPlace = [];
 var maxScenes = 5;
 var actualScenes = 0;
@@ -10,7 +9,6 @@ var remainingPlaces = []; //This array is used to populate the thumnails of rema
 var chosenPlaces = [];
 var allInstructions = [];
 
-// DOM IDS --------
 var mainContainer = document.getElementById('maincontainer'); // where the action happens
 var selectionContainer = document.getElementById('selectioncontainer'); // for 10 destination thumbnails
 var headerContainer = document.getElementById('headercontainer'); // for active image name
@@ -20,7 +18,6 @@ var descriptionContainer = document.getElementById('description'); // for text i
 var eventContainer = document.getElementById('eventcontainer'); // for both forms 
 var finalForm = document.getElementById('finalform'); //for postcard text entry
 
-// CONSTRUCTORS --------
 function Adventure(name, image, text, teaser) {
   this.name = name;
   this.image = `img/${image}`;
@@ -38,22 +35,16 @@ function Instructions(name, image, text) {
   allInstructions.push(this);
 }
 
-// INSTANCES -------
 new Adventure('Space Needle', 'space-needle.png', 'Long Descriptive Text about it', 'Click this awesome choice!');
 new Adventure('Seattle Sunset', 'seattle-sunset.png', 'Long Descriptive Text about SEATTLE', 'Click this awesome choice!');
 new Adventure('Great Wheel', 'great-wheel.png', 'Long Descriptive Text about WHEEL', 'Click this awesome choice!');
 new Adventure('Pike Place', 'pike-place.png', 'Long Descriptive Text about PIKE', 'Click this awesome choice!');
 new Adventure('Waterfront', 'waterfront.png', 'Long Descriptive Text about WATERFRONT', 'Click this awesome choice!');
 
-// for landing page, how to play page, and pre-postcard page
-
 new Instructions('Welcome to our Adventure in Seattle Game!', 'starting-image.png', `If you choose to play, you'll be led on a virtual adventure around the city, to see whichever sights you'd like to see. You'll learn fun facts and trivia about each location along the way. At the end, you'll have a memento from your trip based on where you decided to go! Enjoy your time, hope you love Seattle!`)
 new Instructions('Game play instructions', 'space-needle.png', 'you are having fun right now because you are playing this game')
 new Instructions('Your Custom Postcard Awaits. . .', 'prepostcard.png', 'Click the button to see your memento')
 
-// to neaten up this instantiation we could make the text / blurb/ teasers all variables that get filled by functions. (reference Pheasants demo for this)
-
-// GLOBAL FUNCTIONS ----------
 function renderElement(newElement, parentElement, obj, content, index) {
   if (newElement === 'img' && content === 'thumbnail') { // for thumbnails
     var childElement = document.createElement(newElement);
@@ -79,12 +70,6 @@ function renderElement(newElement, parentElement, obj, content, index) {
   }
 }
 
-function populateDestinations() { // preserves original list of destinations and creates a working copy for user interaction
-  for (var i = 0; i < allDestinations.length; i++) {
-    remainingPlaces[i] = allDestinations[i];
-  }
-}
-
 function beginAdventure() {
   finalForm.setAttribute("style", "display:none;") // hide final button
   // display starting image and give directions
@@ -103,7 +88,9 @@ function inputName(event) {
 }
 
 function enterGame() {
-  populateDestinations();
+  for (var i = 0; i < allDestinations.length; i++) {
+    remainingPlaces[i] = allDestinations[i];
+  } // preserves original list of destinations and creates a working copy for user interaction
   renderElement('h2', headerContainer, allInstructions[1], 'name');
   renderElement('img', imageContainer, allInstructions[1], 'image');
   renderElement('p', descriptionContainer, allInstructions[1], 'text');
@@ -119,21 +106,22 @@ function renderThumbnails() {
 
 function thumbClick(event) {   // user has clicked thumbnail to choose next destination.
   actualScenes++;
-  var clickedDestination = event.target.alt;
-  var popIndex = null;
-  for (var i = 0; i < remainingPlaces.length; i++) {
-    if (clickedDestination === remainingPlaces[i].name) {
-      popIndex = i;
+    if (actualScenes < maxScenes) {    // selection ends after 5 are chosen; users is presented with postcard form 
+    var clickedDestination = event.target.alt;
+    var popIndex = null;
+    for (var i = 0; i < remainingPlaces.length; i++) {
+      if (clickedDestination === remainingPlaces[i].name) {
+        popIndex = i;
+      }
     }
-  }
-  pickedPlace = remainingPlaces.splice(popIndex, 1);
-  chosenPlaces.push(pickedPlace);
-  renderElement('img', eventContainer, pickedPlace[0], 'thumbnail');
-  if (actualScenes === maxScenes) {    // selection ends after 5 are chosen; users is presented with postcard form 
+    pickedPlace = remainingPlaces.splice(popIndex, 1);
+    chosenPlaces.push(pickedPlace);
+    renderElement('img', eventContainer, pickedPlace[0], 'thumbnail');
+    renderThumbnails();
+    showNextScene();    
+  } else {
     postcardInput();
   }
-  renderThumbnails();
-  showNextScene();
 }
 
 function showNextScene() {
@@ -145,12 +133,18 @@ function showNextScene() {
 function flipBigImage(event) {
   // flip image to show trivia about location
   // imageContainer.setAttribute("style", ""); // reveal its .blurb attribute
+  //remove that handler
+  // then add event handler to big image to flip back over. (and then remove that handler and replace it with the flipBigImage one)
 }
 
 function postcardInput() {
-  eventContainer.innerHTML = 'Whew! -- you have completed your journey of Seattle. Each step of your journey has been commemorated by a photo on your custom postcard.';
-  // generate a user input form for the text they'd like to put on the postcard
+  selectionContainer.innerHTML = '';
+  eventContainer.innerHTML = `Whew! -- you have completed your journey of Seattle. Each step of your journey has been commemorated by a photo on your custom postcard. Now it's time to write your custom message. What would you like your postcard to say?`;
+  renderElement('h2', headerContainer, allInstructions[2], 'name');
+  renderElement('img', imageContainer, allInstructions[2], 'image');
+  renderElement('p', descriptionContainer, allInstructions[2], 'text');
   finalForm.setAttribute("style", "display:initial;") // make this button also a link to load new page.
+
   // var postcardButton = document.createElement('a');
   // postcardButton.setAttribute("style", "text-decoration: none; color: black; padding: 5px; margin: 10px; background-color:#ccc; border: 1px solid black;")
   // var nextPage = document.createTextNode('View Postcard')
@@ -158,11 +152,19 @@ function postcardInput() {
   // postcardButton.title = 'Click Here for Your Custom Postcard';
   // postcardButton.href = 'postcard.html';
   // eventContainer.appendChild(postcardButton);
+
+  fillContainerWithSelectedImages();
 }
 
-function fillContainerWithSelectedImages() { //not sure why this doesn't grab the image thumbnails. the idea is to have the complete list populate. 
+function fillContainerWithSelectedImages() { //not sure why this doesn't grab the image thumbnails. the idea is to have the complete list populate and store them to localStorage. 
   for (var i = 0; i < chosenPlaces.length; i++) {
-    renderElement('img', eventContainer, chosenPlaces[i], 'thumbnail', i);
+    var childElement = document.createElement('img');
+    childElement.src = chosenPlaces[i][0].thumbnail;
+    childElement.title = chosenPlaces[i][0].teaser;
+    childElement.alt = chosenPlaces[i][0].name;
+    childElement.setAttribute('id', i);
+    eventContainer.appendChild(childElement);
+  //save chosenPlaces array to localstorage
   }
 }
 
