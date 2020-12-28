765dd806a20dc5df7a2606bf7dd4272eb0bcb180
Stephen Webber <offgridauthor@gmail.com>
2020-12-27 18:32:41 -0800

    added game encounter elements

--- js/app.js
+++ js/app.js
@@ -11,6 +11,16 @@ var allInstructions = [];
 var postcardMessage = '';
 var storedUserName = localStorage.getItem('username');
 
+var date = new Date();
+var postmark = date.toLocaleDateString(); // get local date/time for message postmark
+localStorage.setItem('postmark', postmark);
+
+var allEncounters = [];
+var remainingEncounters = [];
+var doneEncounters = [];
+var currentEncounterIndex = 0;
+var encounterBadges = [];
+
 var mainContainer = document.getElementById('maincontainer'); // where the action happens
 var selectionContainer = document.getElementById('selectioncontainer'); // for 10 destination thumbnails
 var headerContainer = document.getElementById('headercontainer'); // for active image name
@@ -19,8 +29,10 @@ var imageContainer = document.getElementById('imagecontainer'); // for initial i
 var descriptionContainer = document.getElementById('description'); // for text instructions
 var eventContainer = document.getElementById('eventcontainer'); // for both forms
 var finalForm = document.getElementById('finalform'); //for postcard text entry
+var travelledToContainer = document.createElement('section');
+mainContainer.appendChild(travelledToContainer);
 
-function Adventure(name, image, text, blurb, teaser) {
+function Adventure(name, image, text, blurb, teaser) { // teaser on mouseover, blurb on flip, text if clicked
   this.name = name;
   this.image = `img/${image}`;
   this.thumbnail = `thumbs/${image}`;
@@ -37,6 +49,17 @@ function Instructions(name, image, text, extra) {
   this.extra = extra;
   allInstructions.push(this);
 }
+// Positive, Neutral, and Challenge Encounter 
+function Encounter(name, encounter, text, yes, no, yesAttr, noAttr) {
+  this.name = name;
+  this.encounter = `img/${encounter}`;
+  this.text = text;
+  this.yes = yes;
+  this.no = no;
+  this.yesAttr = yesAttr;
+  this.noAttr = noAttr
+  allEncounters.push(this);
+}
 
 new Adventure('Space Needle', 'space-needle.png', 'Long Descriptive Text about it', 'Few people know X about Y.', 'Click this awesome choice!');
 new Adventure('Seattle Sunset', 'seattle-sunset.png', 'Long Descriptive Text about SEATTLE', 'Few people know X about Y.', 'Click this awesome choice!');
@@ -44,38 +67,51 @@ new Adventure('Great Wheel', 'great-wheel.png', 'Long Descriptive Text about WHE
 new Adventure('Pike Place', 'pike-place.png', 'Long Descriptive Text about PIKE', 'Few people know X about Y.', 'Click this awesome choice!');
 new Adventure('Waterfront', 'waterfront.png', 'Long Descriptive Text about WATERFRONT', 'Few people know X about Y.', 'Click THIS awesome choice!');
 
-
 new Instructions('Welcome to our Adventure in Seattle Game!', 'starting-image.png', 'If you choose to play, you\'ll be led on a virtual adventure around the city, to see whichever sights you\'d like to see. You\'ll learn fun facts and trivia about each location along the way. At the end, you\'ll have a memento from your trip based on where you decided to go! Enjoy your time, hope you love Seattle!', 'THIS IS OUR AMAZING STAR WARS SCROLLING INTRO TEXT THIS IS OUR AMAZING STAR WARS SCROLLING INTRO TEXT THIS IS OUR AMAZING STAR WARS SCROLLING INTRO TEXT THIS IS OUR AMAZING STAR WARS SCROLLING INTRO TEXT');
 new Instructions('Game play instructions', 'space-needle.png', 'you are having fun right now because you are playing this game');
 new Instructions('Your Custom Postcard Awaits. . .', 'prepostcard.png', 'Here is where you enter your awesome message');
 new Instructions('Check it out!', 'starting-image.png', 'A virtual memento from your trip. Click the button below if you\'d like to see postcards from previous travellers.');
 new Instructions('Welcome back!', 'starting-image.png', 'If you\'d like to play again, please enter your desired username for this journey.');
 
+// should something like this be stored in separate JSON file instead?
+new Encounter('Street Vendor', 'pike-place.png', 'While you are there, you meet a street vendor selling freshly smoked salmon that smells incredible. He offers you a free sample and its taste is unrivaled. Would you like to take some home with you?', 'The opportunity is too good to pass up. You fork over the money and happily receive your smoked salmon. They\'re going to love it back at home.', 'You decide against the idea of carrying a bag of fish for the rest of your day and continue along your journey.', 'Golden Fish', 'Travel Light');
+new Encounter('Satisfaction Guaranteed', 'pike-place.png', 'As you leave, you pass by a travel agent who flags you down. "Excuse me," she says. "I couldn\'t help but notice that you are from out of town and I just wanted to ask you whether you liked this spot on your Seattle trip. Some people love it, though it\'s not for everyone. Did you enjoy it?', 'Positive Pete', 'Negative Nelly');
+new Encounter('Changing Weather', 'pike-place.png', 'During your time here, a heavy fog rolled in, obscuring your view. Luckily, you were able to capture a great photo beforehand. Are you happy with this image?', 'You are happy with the image and look forward to taking more at your next destination when the weather clears.', 'You shake your head, wishing you\'d had more of a chance to get just the right shot.', 'Positive Pete', 'Negative Nelly');
+new Encounter('Natural Accident', 'pike-place.png', 'While sightseeing, a bird overhead happened to poop on your shoulder. Seeing this, a fellow sightseer standing next to you offers you a spare Seahawks jersey. "It\'s an extra," he says. "I was overzealous and bought more than I\'ll ever need. It\'s yours if you\'d like to have it."\n Do you take the jersey?', 'Sports Fan', 'Bird Lover');
+
 function renderElement(newElement, parentElement, obj, content, index) {
   if (newElement === 'img' && content === 'thumbnail') { // for thumbnails
-    var flipCard = document.createElement('div');
-    flipCard.setAttribute('class', 'flip-card');
-    parentElement.appendChild(flipCard);
-    var inner = document.createElement('div')
-    inner.setAttribute('class', 'flip-card-inner');
-    flipCard.appendChild(inner);
-    var front = document.createElement('div');
-    front.setAttribute('class', 'flip-card-front');
-    inner.appendChild(front);
-    var frontElement = document.createElement(newElement);
-    frontElement.src = obj.thumbnail;
-    frontElement.title = obj.teaser;
-    frontElement.alt = obj.name;
-    frontElement.setAttribute('id', index);
-    frontElement.addEventListener('click', thumbClick);
-    front.appendChild(frontElement);
-    var back = document.createElement('div');
-    back.setAttribute('class', 'flip-card-back');
-    inner.appendChild(back);
-    var flipEl = document.createElement('p')
-    flipEl.textContent = obj.blurb;
-    flipEl.setAttribute('class', 'text'); 
-    back.appendChild(flipEl);
+    if (parentElement === selectionContainer) {
+      var flipCard = document.createElement('div');
+      flipCard.setAttribute('class', 'flip-card');
+      parentElement.appendChild(flipCard);
+      var inner = document.createElement('div')
+      inner.setAttribute('class', 'flip-card-inner');
+      flipCard.appendChild(inner);
+      var front = document.createElement('div');
+      front.setAttribute('class', 'flip-card-front');
+      inner.appendChild(front);
+      var frontElement = document.createElement(newElement);
+      frontElement.src = obj.thumbnail;
+      frontElement.title = obj.teaser;
+      frontElement.alt = obj.name;
+      frontElement.setAttribute('id', index);
+      front.appendChild(frontElement);
+      var back = document.createElement('div');
+      back.setAttribute('class', 'flip-card-back');
+      inner.appendChild(back);
+      back.addEventListener('click', thumbClick);
+      var flipEl = document.createElement('p')
+      flipEl.textContent = obj.blurb;
+      flipEl.setAttribute('class', 'text');
+      back.appendChild(flipEl);
+    } else {
+      var thumb = document.createElement(newElement);
+      thumb.src = obj.thumbnail;
+      thumb.title = obj.teaser;
+      thumb.alt = obj.name;
+      parentElement.appendChild(thumb);
+    }
   } else if (newElement === 'img' && content === 'image') {// for large images
     parentElement.innerHTML = '';
     var childElement = document.createElement(newElement);
@@ -83,7 +119,15 @@ function renderElement(newElement, parentElement, obj, content, index) {
     childElement.title = obj.teaser;
     childElement.alt = obj.name;
     parentElement.appendChild(childElement);
-    childElement.addEventListener('click', flipBigImage);
+    childElement.addEventListener('click', clickBigImage);
+  } else if (newElement === 'img' && content === 'encounter') {// for encounter images
+    parentElement.innerHTML = '';
+    var childElement = document.createElement(newElement);
+    childElement.src = obj.encounter;
+    childElement.title = obj.teaser;
+    childElement.alt = obj.name;
+    parentElement.appendChild(childElement);
+
   } else {
     parentElement.innerHTML = '';
     var childElement = document.createElement(newElement);// for text
@@ -99,7 +143,7 @@ function beginAdventure() {
   imageContainer.innerHTML = '';
   var welcome = document.createElement('p');
   welcome.textContent = allInstructions[0]['extra'];
-//  welcome.setAttribute('class', 'star-wars crawl title');
+  //  welcome.setAttribute('class', 'star-wars crawl');
   imageContainer.appendChild(welcome);
   renderElement('p', descriptionContainer, allInstructions[0], 'text');
   eventContainer.addEventListener('submit', inputName);
@@ -117,6 +161,9 @@ function enterGame() {
   for (var i = 0; i < allDestinations.length; i++) {
     remainingPlaces[i] = allDestinations[i];
   } // preserves original list of destinations and creates a working copy for user interaction
+  for (var i = 0; i < allEncounters.length; i++) {
+    remainingEncounters[i] = allEncounters[i];
+  }
   renderElement('h2', headerContainer, allInstructions[1], 'name');
   renderElement('img', imageContainer, allInstructions[1], 'image');
   renderElement('p', descriptionContainer, allInstructions[1], 'text');
@@ -147,7 +194,7 @@ function thumbClick(event) {   // user has clicked thumbnail to choose next dest
     console.log(actualScenes);
     var stringifiedPlaces = JSON.stringify(chosenPlaces);
     localStorage.setItem('chosenimages', stringifiedPlaces);
-    renderElement('img', eventContainer, pickedPlace[0], 'thumbnail');
+    renderElement('img', travelledToContainer, pickedPlace[0], 'thumbnail');
     renderThumbnails();
     showNextScene();
   } else {
@@ -161,11 +208,61 @@ function showNextScene() {
   renderElement('p', descriptionContainer, pickedPlace[0], 'text');
 }
 
-function flipBigImage(event) {
-  // flip image to show trivia about location
-  // imageContainer.setAttribute("style", ""); // reveal its .blurb attribute
-  //remove that handler
-  // then add event handler to big image to flip back over. (and then remove that handler and replace it with the flipBigImage one)
+function clickBigImage(event) {  // flip image to trigger Encounter
+  event.preventDefault();
+  selectionContainer.innerHTML = ''; // to prevent clicking away
+  currentEncounterIndex = Math.floor(Math.random() * Math.floor(remainingEncounters.length));
+  renderElement('h2', headerContainer, remainingEncounters[currentEncounterIndex], 'name');
+  renderElement('img', imageContainer, remainingEncounters[currentEncounterIndex], 'encounter');
+  renderElement('p', descriptionContainer, remainingEncounters[currentEncounterIndex], 'text');
+  eventContainer.innerHTML = '';
+  var optionYes = document.createElement('a');
+  var optionNo = document.createElement('a');
+  optionNo.setAttribute('style', 'text-decoration: none; color: black; padding: 5px; margin: 10px; background-color:#ccc; border: 1px solid black;');
+  optionYes.setAttribute('style', 'text-decoration: none; color: black; padding: 5px; margin: 10px; background-color:#ccc; border: 1px solid black;');
+  optionYes.title = 'If Yes';
+  optionNo.title = 'If No';
+  var yes = document.createTextNode('Yes');
+  optionYes.appendChild(yes);
+  var no = document.createTextNode('No');
+  optionNo.appendChild(no);
+  eventContainer.appendChild(optionYes);
+  eventContainer.appendChild(optionNo);
+  document.removeEventListener('click', clickBigImage); // prevents accidentally clicking away
+  optionNo.addEventListener('click', encounterButtonNoClick);
+  optionYes.addEventListener('click', encounterButtonYesClick);
+}
+
+function encounterButtonYesClick(event) { // --------- for YES clicks
+  event.preventDefault();
+  eventContainer.innerHTML = '';
+  renderElement('p', descriptionContainer, remainingEncounters[currentEncounterIndex], 'yes');
+  // you earned __
+  eventContainer.innerHTML = 'You earned the ' + remainingEncounters[currentEncounterIndex].yesAttr + ' badge!';
+  encounterBadges.push(currentEncounterIndex.yesAttr)
+  encounterFinished();
+}
+
+function encounterButtonNoClick(event) { // -------- for NO clicks
+  event.preventDefault();
+  eventContainer.innerHTML = '';
+  renderElement('p', descriptionContainer, remainingEncounters[currentEncounterIndex], 'no');
+  encounterBadges.push(currentEncounterIndex.noAttr)
+  eventContainer.innerHTML = 'You earned the ' + remainingEncounters[currentEncounterIndex].noAttr + ' badge!';
+  encounterFinished();
+}
+
+function encounterFinished() {
+  //"choose your next destination"
+  headerContainer.innerHTML = '';
+  var next = document.createElement('h2');
+  next.textContent = 'Click any thumbnail to select your next destination.';
+  headerContainer.appendChild(next);
+  renderThumbnails();
+}
+
+function tallyEncounterBadges() { // -------- at end, to store and render with postcard.
+  console.log(encounterBadges);
 }
 
 function postcardInput() {
@@ -179,7 +276,6 @@ function postcardInput() {
 }
 
 function postcardPull(event) {
-  console.log('Inside postcardpull');
   event.preventDefault();
   postcardMessage = event.target.postcardinput.value;
   localStorage.setItem('postcardmessage', postcardMessage);
@@ -193,7 +289,7 @@ function revealPostcard() {
   renderElement('h2', headerContainer, allInstructions[3], 'name');
   renderElement('p', descriptionContainer, allInstructions[3], 'text');
   var cardTextEl = document.createElement('p');
-  cardTextEl.textContent = postcardMessage;
+  cardTextEl.textContent = postcardMessage + ' ' + postmark;
   cardTextEl.setAttribute('id', 'postcardmessage');
   imageContainer.appendChild(cardTextEl);
   var cardFromEl = document.createElement('p');
@@ -221,7 +317,7 @@ if (storedUserName) {
   imageContainer.innerHTML = '';
   var welcome = document.createElement('p');
   welcome.textContent = allInstructions[0]['extra'];
-//  welcome.setAttribute('class', 'star-wars crawl title');
+  //  welcome.setAttribute('class', 'star-wars crawl');
   imageContainer.appendChild(welcome);
   // renderElement('img', imageContainer, allInstructions[4], 'image');
   renderElement('p', descriptionContainer, allInstructions[4], 'text');
--- js/results.js
+++ js/results.js
@@ -3,15 +3,17 @@
 var storedUserName = localStorage.getItem('username');
 var storedImages = localStorage.getItem('chosenimages');
 var storedPostcardMessage = localStorage.getItem('postcardmessage');
-var previousCardsToDisplay = 4;
-var postcardImages = ['thumbs/space-needle.png', 'thumbs/seattle-sunset.png', 'thumbs/great-wheel.png', 'thumbs/pike-place.png', 'thumbs/waterfront.png']
+var postmark = localStorage.getItem('postmark');
+var previousCardsToDisplay = 5;
+var postcardImages = ['thumbs/space-needle.png', 'thumbs/seattle-sunset.png', 'thumbs/great-wheel.png', 'thumbs/pike-place.png', 'thumbs/waterfront.png'];
 var previousMessages = ['I got hit in the face by a fish but it\'s all good!', 'Wow! Changed my life. I\'ll never forget this trip.', 'With love from Seattle!', 'Did you realize there are over 100 words for fog in Seattle?', 'I can\'t wait to get back to Seattle someday.', 'It\'s cold and dark and I want to come home.'];
+var previousPostmarks = ['11/11/1997', '5/13/2010', '2/1/2017', '9/22/2019', '12/31/1892'];
 var previousSenders = ['Stephen', 'Carly', 'Clement', 'Mikey', 'Herman Melville'];
 var postContainer = document.getElementById('allcards');
 
 function makeRandom() {
   return Math.floor(Math.random() * Math.floor(postcardImages.length));
-} // stretch goal: to ensure non-duplicates with these randoms
+} 
 
 function renderElement(newElement, obj, content) {
   if (newElement === 'img' && content === 'thumbnail') { // for thumbnails
@@ -36,33 +38,67 @@ function showStoredPostcard() {
   // render the user's postcard
 
 
-      // var retrievedMessage = localStorage.getItem('postcardmessage');
-    // var retrievedImages = localStorage.getItem('chosenimages');
-    // storedPostcardMessage = JSON.parse(retrievedMessage);
-    // storedPostcardImages = JSON.parse(retrievedImages);
-    // var retrievedName = localStorage.getItem('username');
-    // storedUserName = JSON.parse(retrievedName);
-    // // renders the postcard. (plus a timestamp would be cool).
-    // for (var i = 0; i < storedPostcardImages.length; i++) {
-    //   var childElement = document.createElement('img');
-    //   childElement.src = storedPostcardImages[i].thumbnail;
-    //   eventContainer.appendChild(childElement);
-    // }
+  // var retrievedMessage = localStorage.getItem('postcardmessage');
+  // var retrievedImages = localStorage.getItem('chosenimages');
+  // storedPostcardMessage = JSON.parse(retrievedMessage);
+  // storedPostcardImages = JSON.parse(retrievedImages);
+  // var retrievedName = localStorage.getItem('username');
+  // storedUserName = JSON.parse(retrievedName);
+  // // renders the postcard. (plus a timestamp would be cool).
+  // for (var i = 0; i < storedPostcardImages.length; i++) {
+  //   var childElement = document.createElement('img');
+  //   childElement.src = storedPostcardImages[i].thumbnail;
+  //   eventContainer.appendChild(childElement);
+  // }
 
 
 }
 
 function showPreviousPostcards() {
   for (var i = 0; i < previousCardsToDisplay; i++) {
-    for (var j = 0; j < 5; j++) {
-      var randomIndex = makeRandom();
-      renderElement('img', postcardImages[randomIndex], 'thumbnail');
-    }
+    renderPreviousCardImages();
     renderElement('p', previousMessages[i], 'message');
     renderElement('p', previousSenders[i], 'sender');
-
-
+    renderElement('p', previousPostmarks[i], 'postmark');
   }
 }
+
+function renderPreviousCardImages() {
+  var queue = []; // for ensuring random 5
+  while (queue.length < 5) {
+    var tempIndex = makeRandom(postcardImages.length);
+    while (queue.includes(tempIndex)) {
+      tempIndex = makeRandom(postcardImages.length);
+    }
+    queue.push(tempIndex);
+  }
+  var allPostcardsContainer = document.getElementById('allcards'); // main body
+  var postCard = document.createElement('section');
+  allPostcardsContainer.appendChild(postCard); // new card container
+  var imageOneEl = document.createElement('img'); // images
+  var imageTwoEl = document.createElement('img');
+  var imageThreeEl = document.createElement('img');
+  var imageFourEl = document.createElement('img');
+  var imageFiveEl = document.createElement('img');
+  var imageOne = queue.shift();
+  var imageTwo = queue.shift();
+  var imageThree = queue.shift();
+  var imageFour = queue.shift();
+  var imageFive = queue.shift();
+  imageOneEl.src = postcardImages[imageOne];
+  imageTwoEl.src = postcardImages[imageTwo];
+  imageThreeEl.src = postcardImages[imageThree];
+  imageFourEl.src = postcardImages[imageFour];
+  imageFiveEl.src = postcardImages[imageFive];
+  postCard.appendChild(imageOneEl);
+  postCard.appendChild(imageTwoEl);
+  postCard.appendChild(imageThreeEl);
+  postCard.appendChild(imageFourEl);
+  postCard.appendChild(imageFiveEl);
+}
+
+
 //showStoredPostcard();
 showPreviousPostcards();
+
+
